2b02f725d9f500e816549d188ca40523cd872be0
89acat <tamara.jevtic@student.um.si>
2024-04-03 13:56:54 +0200

    Test Fix

--- src/test/java/si/um/feri/demo/rest/GradeResourceTest.java
+++ src/test/java/si/um/feri/demo/rest/GradeResourceTest.java
@@ -1,26 +1,32 @@
 package si.um.feri.demo.rest;
 
-import io.quarkus.test.junit.QuarkusTest;
-import io.restassured.RestAssured;
-import org.junit.jupiter.api.BeforeEach;
+import io.smallrye.mutiny.Uni;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import si.um.feri.demo.dao.GradeRepository;
+import si.um.feri.demo.vao.Grade;
+import java.util.Collections;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
-import static io.restassured.RestAssured.given;
-
-@QuarkusTest
 public class GradeResourceTest {
 
-    @BeforeEach
-    public void setup() {
-        RestAssured.port = 8080;
-    }
-
     @Test
-    public void testGetAllGradesEndpoint() {
-        given()
-                .when().get("/grades")
-                .then()
-                .statusCode(200);
-    }
+    public void testListAllGrades() {
+        Grade testGrade = new Grade();
+        testGrade.studentId = "123";
+        testGrade.professorId = "456";
+        testGrade.subjectId = "789";
+        testGrade.gradeValue = 8;
+        testGrade.description = "Description1";
 
+
+        GradeRepository gradeRepository = Mockito.mock(GradeRepository.class);
+        when(gradeRepository.listAllGrades()).thenReturn(Uni.createFrom().item(Collections.singletonList(testGrade)));
+
+        List<Grade> result = gradeRepository.listAllGrades().await().indefinitely();
+        assertEquals(1, result.size());
+        assertEquals(testGrade, result.getFirst());
+    }
 }
