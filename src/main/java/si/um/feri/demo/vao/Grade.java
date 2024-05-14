774f288f2d68882989778182beb5b78a52c4a74f
89acat <tamara.jevtic@student.um.si>
2024-05-14 19:04:58 +0200

    Adding ID object to model Grade

--- src/main/java/si/um/feri/demo/vao/Grade.java
+++ src/main/java/si/um/feri/demo/vao/Grade.java
@@ -1,8 +1,10 @@
 package si.um.feri.demo.vao;
 
 import io.quarkus.mongodb.panache.reactive.ReactivePanacheMongoEntity;
+import org.bson.types.ObjectId;
 
 public class Grade extends ReactivePanacheMongoEntity {
+    public ObjectId id;
     public String studentId;
     public String professorId;
     public String subjectId;
--- src/main/java/si/um/feri/demo/vao/GradeDTO.java
+++ src/main/java/si/um/feri/demo/vao/GradeDTO.java
@@ -1,6 +1,9 @@
 package si.um.feri.demo.vao;
 
+import org.bson.types.ObjectId;
+
 public class GradeDTO {
+    public ObjectId id;
     public String studentId;
     public String professorId;
     public String subjectId;
@@ -9,6 +12,7 @@ public class GradeDTO {
 
 
     public GradeDTO(Grade grade) {
+        this.id = grade.id;
         this.studentId = grade.studentId;
         this.professorId = grade.professorId;
         this.subjectId = grade.subjectId;
